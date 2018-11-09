208a17221554d320f9d1da891acdfd6e988ff717
Antonio Marcos Ferreira <amfcode@gmail.com>
2018-11-09 20:09:05 -0200

    Aplica correções dos exercícios

--- hadoop_map_reduce/analyzing-subway-data/reducer.py
+++ hadoop_map_reduce/analyzing-subway-data/reducer.py
@@ -15,14 +15,14 @@ def reducer():
         unit, entries = data
 
         if old_unit and old_unit != unit:
-            print old_unit, "\t", total_entries
+            print old_unit, "\t", int(total_entries)
             total_entries = 0
 
         old_unit = unit
-        total_entries += 1
+        total_entries += float(entries)
 
     if old_unit != None:
-        print old_unit, "\t", total_entries
+        print old_unit, "\t", int(total_entries)
         
 sys.stdout = open('reducer_result.txt', 'w')
 reducer()
